cea26079f70419c1aca320022378663cf3f79888
Bartosz Telenczuk <bartosz.telenczuk-ext@aphp.fr>
2020-01-08 02:00:03 +0100

    add script for batch training for multiple services

--- tf_codage/__init__.py
+++ tf_codage/__init__.py
@@ -1,6 +1,8 @@
 import os
+import GPUtil
 os.environ['CUDA_DEVICE_ORDER'] = os.environ.get('CUDA_DEVICE_ORDER', 'PCI_BUS_ID')
-os.environ['CUDA_VISIBLE_DEVICES'] = os.environ.get('CUDA_VISIBLE_DEVICES', '0,1,2,3')
+available_devices = str(GPUtil.getFirstAvailable(maxMemory=0.1)[0])
+os.environ['CUDA_VISIBLE_DEVICES'] = os.environ.get('CUDA_VISIBLE_DEVICES', available_devices)
 
 def print_env():
     """Dump environment to the standard output"""
