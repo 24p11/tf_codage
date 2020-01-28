b31ce2aef1970c6ad87fa0f04d51cf19c9a9b561
Bartosz Telenczuk <bartosz.telenczuk-ext@aphp.fr>
2020-01-28 11:39:53 +0100

    added unit test for data module

---
+++ tests/test_data.py
@@ -0,0 +1,8 @@
+from tf_codage import data
+
+def test_multilabel_dataset():
+    df, num_labels = data.make_multilabel_dataframe("tests/cro_data.csv", min_examples=0)
+    assert len(df) == 2
+    assert num_labels == 3
+    assert df.target[0] == {'LDKA900'}
+    assert df.target[1] == {'AELB001','ZZLP025'}
