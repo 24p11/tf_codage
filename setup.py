bb1af84542c4629bca75e1b4593a2400592875aa
Bartosz Telenczuk <bartosz@telenczuk.pl>
2020-01-29 22:57:15 +0100

    add dependencies

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ setup(
     name="TensorFlow Codage",
     version="0.1",
     packages=['tf_codage'],
+    install_requires=[
+        'GPUtil',
+        'matplotlib',
+        'tensorflow>=2.0',
+        'transformers>=2.2'],
     entry_points = {
         'console_scripts': ['download_hdfs_csv=tf_codage.cli.download_csv:main']
     }
