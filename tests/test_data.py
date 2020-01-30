48a177ce1ac02a5de7a6907217e17ff44d03622d
Bartosz Telenczuk <bartosz.telenczuk-ext@aphp.fr>
2020-01-30 01:09:00 +0100

    adding unit tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,7 @@
 from tf_codage import data
 
 def test_multilabel_dataset():
+    "test importing multilabel data into dataframe"
     df, num_labels = data.make_multilabel_dataframe("tests/cro_data.csv", min_examples=0)
     assert len(df) == 2
     assert num_labels == 3
