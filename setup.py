2a5bf7b6d480035f84a836b3fe890dbdfd4c1123
Bartosz Telenczuk <bartosz@telenczuk.pl>
2020-02-06 11:34:04 +0100

    add pyarrow to dependencies

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
         'tensorflow>=2.0',
         'transformers>=2.2',
         'scikit-learn>=0.21.3',
-        'pandas>=0.25.3'],
+        'pandas>=0.25.3',
+        'pyarrow>=0.15.1'],
     entry_points = {
         'console_scripts': ['download_hdfs_csv=tf_codage.cli.download_csv:main']
     }
