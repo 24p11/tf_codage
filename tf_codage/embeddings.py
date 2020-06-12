9430e1842f091443134782f4f9f8fc3dcff95f3a
Bartosz Telenczuk <bartosz.telenczuk-ext@aphp.fr>
2020-06-12 15:48:36 +0200

    adding sentence embeddings module

--- tf_codage/embeddings.py
+++ tf_codage/embeddings.py
@@ -9,6 +9,8 @@ from itertools import groupby
 
 import tensorflow as tf
 import numpy as np
+import joblib
+from pathlib import Path
 
 
 def grouper(iterable, n, fillvalue=None):
@@ -17,6 +19,25 @@ def grouper(iterable, n, fillvalue=None):
     args = [iter(iterable)] * n
     return zip_longest(fillvalue=fillvalue, *args)
 
+        
+        
+def generator_to_dataset(generator_factory, batch_size):
+    dataset = tf.data.Dataset.from_generator(
+        generator_factory,
+        output_types=(
+            {'input_ids': tf.int32,
+             'token_type_ids': tf.int32,
+             'attention_mask': tf.int32},
+             tf.int32),
+        output_shapes=(
+            {"input_ids": [510],
+             "token_type_ids": [510],
+             "attention_mask": [510]}, []))
+
+    dataset = dataset.batch(batch_size, drop_remainder=False)
+    dataset = dataset.prefetch(1)
+    return dataset
+    
 class SentenceEmbedder:
     
     SPLIT_SIZE = 10000
@@ -24,10 +45,30 @@ class SentenceEmbedder:
     MAX_SENTENCES = 510
     
     def __init__(self, sentence_tokenizer, word_tokenizer, model):
-        self.model = model
+        self._model = model
         self.sentence_tokenizer = sentence_tokenizer
         self.word_tokenizer = word_tokenizer
+        
+    def _calculate_embeddings(self, dataset):
+        
+        model = self.model
 
+        embeddings = []
+        for inputs, text_ids in dataset:
+            outputs = model(inputs).numpy()
+            embeddings += list(zip(outputs, text_ids.numpy()))
+
+        text_groups = groupby(embeddings, key=lambda x: x[1])
+
+        for text_id, sentences_ids in text_groups:
+            sentences = list(map(lambda x: x[0], sentences_ids))
+            text_embeddings = np.vstack(sentences)
+            yield text_embeddings, text_id
+
+    @property
+    def model(self):
+        return self._model
+    
     def encode(self, txt, txt_id):
         
         sentences = self.sentence_tokenizer.tokenize(txt)[:self.MAX_SENTENCES]
@@ -41,9 +82,25 @@ class SentenceEmbedder:
                 pad_to_max_length=True,
                 return_tensors=None)
             yield token_ids, txt_id
-
+            
     def make_sentence_datasets(self, texts):
         """Prepare dataset with tokenized sentences and words."""
+        
+        def add_progress_bar(generator_factory):
+            
+            def _generator_with_progress():
+                for encoded_text in generator_factory():
+                    yield encoded_text
+            return _generator_with_progress
+        
+        for generator in self._split_encode_texts(texts):
+            generator = add_progress_bar(generator)
+            dataset = generator_to_dataset(generator, self.EMBEDDING_BATCH_SIZE)
+            yield dataset
+        pbar.close()
+    
+            
+    def _split_encode_texts(self, texts):
         
         indexed_texts = zip(texts, range(len(texts)))
         text_groups = grouper(indexed_texts, self.SPLIT_SIZE, fillvalue=("", -1))
@@ -62,40 +119,52 @@ class SentenceEmbedder:
         for text_group in text_groups:
 
             generator = make_generator(text_group)
-
-            dataset = tf.data.Dataset.from_generator(
-                generator,
-                output_types=(
-                    {'input_ids': tf.int32,
-                     'token_type_ids': tf.int32,
-                     'attention_mask': tf.int32},
-                     tf.int32),
-                output_shapes=(
-                    {"input_ids": [510],
-                     "token_type_ids": [510],
-                     "attention_mask": [510]}, []))
-
-            dataset = dataset.batch(self.EMBEDDING_BATCH_SIZE, drop_remainder=False)
-            dataset = dataset.prefetch(1)
-            yield dataset
+            yield generator
         pbar.close()
-
 
     def transform(self, all_texts):
 
         datasets = self.make_sentence_datasets(all_texts)
 
-        for j, dataset in enumerate(datasets):
-            data = []
-
-            embeddings = []
-            for inputs, text_ids in dataset:
-                outputs = self.model(inputs).numpy()
-                embeddings += list(zip(outputs, text_ids.numpy()))
-
-            text_groups = groupby(embeddings, key=lambda x: x[1])
-
-            for text_id, sentences_ids in text_groups:
-                sentences = list(map(lambda x: x[0], sentences_ids))
-                text_embeddings = np.vstack(sentences)
+        for _, dataset in enumerate(datasets):
+            for text_embeddings, text_id in self._calculate_embeddings(dataset):
                 yield text_embeddings, all_texts.iloc[text_id]
+                
+from tf_codage.models import TFCamembertForSentenceEmbedding
+class SentenceEmbedderDistributed(SentenceEmbedder):
+    """Sentence embedder using dask-distributed to spread task across GPUs on local machine"""
+    
+    def __init__(self, sentence_tokenizer, word_tokenizer, model_dir):
+        self._model_cls = TFCamembertForSentenceEmbedding
+        self._model_dir = model_dir
+        self.sentence_tokenizer = sentence_tokenizer
+        self.word_tokenizer = word_tokenizer
+
+    @staticmethod
+    def _run_and_save(model_future, text_group, dataset_id, output_dir=Path('.'), prefix="", batch_size=32):
+        
+        model = model_future
+        
+        dataset = generator_to_dataset(text_group.__iter__, batch_size)
+ 
+        data = list(_calculate_embeddings(model, dataset)) 
+        output_file = output_dir /  '{}embeddings_{:03d}.joblib'.format(prefix, dataset_id)
+        joblib.dump(data, output_file)
+ 
+    def transform_distributed(self, all_texts, output_dir, client):
+        
+        encoded_texts = self._split_encode_texts(all_texts)
+        model_future = client.submit(
+            self._model_cls.from_pretrained, self._model_dir)
+        
+        stream = TeeStream(sys.stdout, sys.__stdout__)
+        pbar = tqdm(total=len(all_texts), file=stream)
+        
+        futures = []
+        for j, text_generator in enumerate(encoded_texts):
+            client.wait_for_workers(1)
+            text_group = list(text_generator())
+            pbar.update(len(text_group))
+            futures.append(client.submit(self._run_and_save, model_future, text_group, j, output_dir, "", self.EMBEDDING_BATCH_SIZE))
+        client.gather(futures)
+        pbar.close()
