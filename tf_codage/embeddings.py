26bf8581bc9f136cd2700b1ddf7003265485fbd7
Bartosz Telenczuk <bartosz.telenczuk-ext@aphp.fr>
2020-07-01 13:52:46 +0200

    refactor embeddings module

--- tf_codage/embeddings.py
+++ tf_codage/embeddings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 
 
 def grouper(iterable, n, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+    "collect data into fixed-length chunks or blocks"
+    # grouper('abcdefg', 3, 'x') --> abc def gxx
     args = [iter(iterable)] * n
     return zip_longest(fillvalue=fillvalue, *args)
 
@@ -97,22 +97,17 @@ class SentenceEmbedder:
             generator = add_progress_bar(generator)
             dataset = generator_to_dataset(generator, self.EMBEDDING_BATCH_SIZE)
             yield dataset
-        pbar.close()
-    
             
     def _split_encode_texts(self, texts):
         
         indexed_texts = zip(texts, range(len(texts)))
         text_groups = grouper(indexed_texts, self.SPLIT_SIZE, fillvalue=("", -1))
-        stream = TeeStream(sys.stdout, sys.__stdout__)
-        pbar = tqdm(total=len(texts), file=stream)
         def make_generator(gen_texts):
 
             def generator_factory():
                 for text, i in gen_texts:
                     if i > -1:
                         yield from self.encode(text, i)
-                    pbar.update(1)
 
             return generator_factory
 
@@ -120,7 +115,6 @@ class SentenceEmbedder:
 
             generator = make_generator(text_group)
             yield generator
-        pbar.close()
 
     def transform(self, all_texts):
 
@@ -128,43 +122,4 @@ class SentenceEmbedder:
 
         for _, dataset in enumerate(datasets):
             for text_embeddings, text_id in self._calculate_embeddings(dataset):
-                yield text_embeddings, all_texts.iloc[text_id]
-                
-from tf_codage.models import TFCamembertForSentenceEmbedding
-class SentenceEmbedderDistributed(SentenceEmbedder):
-    """Sentence embedder using dask-distributed to spread task across GPUs on local machine"""
-    
-    def __init__(self, sentence_tokenizer, word_tokenizer, model_dir):
-        self._model_cls = TFCamembertForSentenceEmbedding
-        self._model_dir = model_dir
-        self.sentence_tokenizer = sentence_tokenizer
-        self.word_tokenizer = word_tokenizer
-
-    @staticmethod
-    def _run_and_save(model_future, text_group, dataset_id, output_dir=Path('.'), prefix="", batch_size=32):
-        
-        model = model_future
-        
-        dataset = generator_to_dataset(text_group.__iter__, batch_size)
- 
-        data = list(_calculate_embeddings(model, dataset)) 
-        output_file = output_dir /  '{}embeddings_{:03d}.joblib'.format(prefix, dataset_id)
-        joblib.dump(data, output_file)
- 
-    def transform_distributed(self, all_texts, output_dir, client):
-        
-        encoded_texts = self._split_encode_texts(all_texts)
-        model_future = client.submit(
-            self._model_cls.from_pretrained, self._model_dir)
-        
-        stream = TeeStream(sys.stdout, sys.__stdout__)
-        pbar = tqdm(total=len(all_texts), file=stream)
-        
-        futures = []
-        for j, text_generator in enumerate(encoded_texts):
-            client.wait_for_workers(1)
-            text_group = list(text_generator())
-            pbar.update(len(text_group))
-            futures.append(client.submit(self._run_and_save, model_future, text_group, j, output_dir, "", self.EMBEDDING_BATCH_SIZE))
-        client.gather(futures)
-        pbar.close()
+                yield text_embeddings, all_texts.iloc[text_id], all_texts.index[text_id]
